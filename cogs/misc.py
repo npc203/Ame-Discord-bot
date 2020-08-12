d0008bd813586a223e135ecc7a0a9fe224dfbfd1
npc203 <npc203@users.noreply.github.com>
2020-08-13 00:18:24 +0530

    reduced abuse

--- cogs/misc.py
+++ cogs/misc.py
@@ -5,17 +5,21 @@ import random,requests
 class Miscellaneous(Cog):
     def __init__(self,bot):
         self.perms=("Admin","Owner")
+        self.limit=50
     @commands.command(help='Meant for Pinging the given name on every available text channel \n only epic people can use it')
     async def punish(self,ctx,name,times):
         if len([x for x in [str(i.name) for i in ctx.message.author.roles] if x in self.perms])>0:
                 await ctx.message.delete()
-                for channel in ctx.guild.text_channels:
-                    try:
-                        for _ in range(int(times)):
-                            await channel.send(name)
-                        await channel.purge(limit=int(times))
-                    except:
-                        print("Skipped: #",channel,sep='')
+                if times>self.limit:
+                    await ctx.send("Pls don't abuse me senpai uwu")
+                else:
+                    for channel in ctx.guild.text_channels:
+                        try:
+                            for _ in range(int(times)):
+                                await channel.send(name)
+                            await channel.purge(limit=int(times))
+                        except:
+                            print("Skipped: #",channel,sep='')
         else:
             print(type(ctx.message.author),ctx.message.author)
             await ctx.send("```***There was an attempt***```")
@@ -24,12 +28,15 @@ class Miscellaneous(Cog):
     @commands.command(help='Cleans the mess made by punish command \n only epic people can use it')
     async def clean(self,ctx,times):
         if len([x for x in [str(i.name) for i in ctx.message.author.roles] if x in self.perms])>0:
-                await ctx.message.delete()        
-                for channel in ctx.guild.text_channels:
-                    try:
-                        await channel.purge(limit=int(times))
-                    except:
-                        print("Skipped: #",channel,sep='')
+                await ctx.message.delete()    
+                if times>self.limit:
+                    await ctx.send("Pls don't abuse me senpai uwu") 
+                else:   
+                    for channel in ctx.guild.text_channels:
+                        try:
+                            await channel.purge(limit=int(times))
+                        except:
+                            print("Skipped: #",channel,sep='')
         else:
             #print(type(ctx.message.author),ctx.message.author)
             await ctx.send("```***There was an attempt***```")
@@ -40,15 +47,21 @@ class Miscellaneous(Cog):
         #print([str(i.name) for i in ctx.message.author.roles],self.perms )
         if len([x for x in [str(i.name) for i in ctx.message.author.roles] if x in self.perms])>0:
             await ctx.message.delete()
-            await ctx.channel.purge(limit=int(limit))
+            if limit>self.limit:
+                await ctx.send("Pls don't abuse me senpai uwu")
+            else:
+                await ctx.channel.purge(limit=int(limit))
         else:
             await ctx.send("***There was an attempt***")
     
     @commands.command(help='SPAMS \n only epic people can use it')
-    async def spam(self,ctx,text,limit:int):
+    async def spam(self,ctx,*args):
         if len([x for x in [str(i.name) for i in ctx.message.author.roles] if x in self.perms])>0:
-            for i in range(limit):
-                await ctx.send(text)
+            if int(args[-1]) > self.limit:
+                await ctx.send("Pls don't abuse me senpai uwu")
+            else:
+                for i in range(int(args[-1])):
+                    await ctx.send(' '.join(args[:-1]))
         else:
             await ctx.send("***There was an attempt***")
         
