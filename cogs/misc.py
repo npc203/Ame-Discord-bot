9a36d34e6fb6db2a9a1dc3eed1fcbf384736d040
npc203 <npc203@users.noreply.github.com>
2020-08-12 19:51:24 +0530

    bug fixes

--- cogs/misc.py
+++ cogs/misc.py
@@ -8,7 +8,7 @@ class Miscellaneous(Cog):
         self.perms=("Admin","Owner")
     @commands.command(help='Meant for Pinging the given name on every available text channel \n only epic people can use it')
     async def punish(self,ctx,name,times):
-        if len((x for x in (str(i.name) for i in ctx.message.author.roles) if x in self.perms))>0:
+        if len([x for x in [str(i.name) for i in ctx.message.author.roles] if x in self.perms])>0:
                 await ctx.message.delete()
                 for channel in ctx.guild.text_channels:
                     try:
@@ -24,7 +24,7 @@ class Miscellaneous(Cog):
     
     @commands.command(help='Cleans the mess made by punish command \n only epic people can use it')
     async def clean(self,ctx,times):
-        if len((x for x in (str(i.name) for i in ctx.message.author.roles) if x in self.perms))>0:
+        if len([x for x in [str(i.name) for i in ctx.message.author.roles] if x in self.perms])>0:
                 await ctx.message.delete()        
                 for channel in ctx.guild.text_channels:
                     try:
@@ -39,7 +39,7 @@ class Miscellaneous(Cog):
     @commands.command(help='Deletes messages upto the given limit in the present channel \n only epic people can use it')
     async def purge(self,ctx,limit):
         #print([str(i.name) for i in ctx.message.author.roles],self.perms )
-        if len((x for x in (str(i.name) for i in ctx.message.author.roles) if x in self.perms))>0:
+        if len([x for x in [str(i.name) for i in ctx.message.author.roles] if x in self.perms])>0:
             await ctx.message.delete()
             await ctx.channel.purge(limit=int(limit))
         else:
@@ -47,7 +47,7 @@ class Miscellaneous(Cog):
     
     @commands.command(help='SPAMS \n only epic people can use it')
     async def spam(self,ctx,text,limit:int):
-        if len((x for x in (str(i.name) for i in ctx.message.author.roles) if x in self.perms))>0:
+        if len([x for x in [str(i.name) for i in ctx.message.author.roles] if x in self.perms])>0:
             for i in range(limit):
                 await ctx.send(text)
         else:
